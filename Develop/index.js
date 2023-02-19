4ceaf3ba20397c8703e7f257210310fd132e1770
Andrea Ramirez <andrea.anza3@gmail.com>
2023-02-19 16:50:00 -0600

    Added array of questions, installed inquirer and the prompt is working correctly. Also added structure to the readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,54 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    type: 'input',
+    name: 'title',
+    message: "TITLE: What's your project title?",
+    },
+    {
+    type: 'input',
+    name: 'description',
+    message: 'DESCRIPTION: Write a description for your project',
+    },
+    {
+    type: 'input',
+    name: 'installation',
+    message:
+        'INSTALLATION: What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+    },
+    {
+    type: 'input',
+    name: 'usage',
+    message: 'USAGE: Provide instructions and examples for use',
+    },
+    {
+    type: 'input',
+    name: 'license',
+    message:
+        'This lets other developers know what they can and cannot do with your project. If you need help choosing a license',
+    },
+    ];
+
+inquirer.prompt(questions).then((answers) => {
+    console.log(JSON.stringify(answers, null, '  '));
+});
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+//     fs.writeFile('README.md', questions, data)
+// }
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
+// inquirer.prompt(questions)
+// .then(data);
+// writeToFile()
+
 
 // Function call to initialize app
-init();
+// init();
+
--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# Professional README Generator Starter Code
+# %Title%
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+## Description
+%Description%
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+%Installation%
+
+## Usage
+%Usage%
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+## License
+%License%
+
